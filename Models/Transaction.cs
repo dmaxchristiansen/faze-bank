6d00d00d202c0db084ceffd4f85bf900f6a94ad2
Daniel Christiansen <dmaxchristiansen@gmail.com>
2020-01-17 10:46:40 -0700

    first commit

---
+++ Models/Transaction.cs
@@ -0,0 +1,42 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BankAccounts.Models
+{
+    [Table("transactions")]
+    public class Transaction
+    {
+        //************************************************* */
+        [Key]
+        public int TransactionId {get;set;}
+        //************************************************* */
+
+
+        //************************************************* */
+        [Required]
+        [Display(Name="Depoist/Withdraw:")]
+        public decimal Amount {get;set;}
+        //************************************************* */
+
+
+        //************************************************* */
+        public int UserId {get;set;}
+        //************************************************* */
+
+
+        //************************************************* */
+        public User User;
+        //************************************************* */
+
+
+        //************************************************* */
+        public DateTime CreatedAt {get;set;} = DateTime.Now;
+        //************************************************* */
+
+
+        //************************************************* */
+        public DateTime UpdatedAt {get;set;} = DateTime.Now;
+        //************************************************* */
+    }
+}
